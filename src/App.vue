d98ae4a3d4e0bdcb7ec389aed10f3069a9e2d571
GeXing <gexing@purvar.com>
2022-01-27 14:21:06 +0800

    upt: update smt

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,31 @@
 </template>
 
 <script>
-import { copyText } from "@iamgx/purvar-utils";
+function copyText(content) {
+  if (!content) return;
+
+  let eleTextarea = document.querySelector("#tempTextarea");
+  if (!eleTextarea && !navigator.clipboard) {
+    eleTextarea = document.createElement("textarea");
+    eleTextarea.style.width = 0;
+    eleTextarea.style.position = "fixed";
+    eleTextarea.style.left = "-999px";
+    eleTextarea.style.top = "10px";
+    document.body.appendChild(eleTextarea);
+  }
+
+  const funCopy = function (text) {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(text);
+    } else {
+      eleTextarea.value = text;
+      eleTextarea.select();
+      document.execCommand("copy", true);
+    }
+  };
+
+  funCopy(content);
+}
 import Doc from "@/components/doc";
 export default {
   name: "App",
