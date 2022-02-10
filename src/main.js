e1b1a0828ca4ea19da489a15e46efffaec9a0b24
GeXing <gexing@purvar.com>
2022-02-10 13:51:29 +0800

    upt: 增加qiankun微应用测试配置

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Markdown from '@/components/markdown.vue'
-Vue.config.productionTip = false
-Vue.prototype.marked = text => {
-  if (!window.markdownit) return text
-  const md = new window.markdownit()
-  return md.render(text)
+if (window.__POWERED_BY_QIANKUN__) {
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
 }
-Vue.component(Markdown.name, Markdown)
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+import Vue from 'vue'
+import App from '@/App'
+import Markdown from '@/components/markdown.vue'
+
+let instance = null
+
+const render = () => {
+  Vue.config.productionTip = false
+  Vue.component(Markdown.name, Markdown)
+  instance = new Vue({ el: '#app', render: h => h(App) })
+}
+
+!window.__POWERED_BY_QIANKUN__ && render()
+
+export const bootstrap = async () => console.log(' bootstraped')
+
+export const mount = async props => render(props)
+
+export const unmount = async () => {
+  instance.$destroy()
+  instance = null
+}
