67aaeb9494bb053a1484de50d72bf7757b671986
GeXing <gexing@purvar.com>
2022-02-11 11:22:43 +0800

    upt: docs sort

--- src/data.js
+++ src/data.js
@@ -1,108 +1,29 @@
 export default [
-  {
-    title: '修改 Mac Launchpad 图标的大小',
-    date: '2020-02-24 22:06:41',
-    description: `(大碗宽面MacBook版) \n硬盘不大~, 但要还是要使劲儿下, 就算屏幕装不下~, 没关系还有这招啊🤪`,
-    path: 'modify-mac-dock.md'
-  },
-  {
-    title: 'webpack4 完整配置文件记录',
-    date: '2020-02-24 17:51:12',
-    path: 'webpack4-full-config.md'
-  },
+  { title: '动态设置elementui主题', date: '2022-02-08 17:43:22', path: 'element-ui-theme-set.vue' },
+  { title: '2021, 有喜有悲的一年', date: '2022-01-31 00:13:22', path: '2021-unhappy.vue' },
+  { title: '项目部署新版本后怎么提示用户刷新浏览器？', date: '2022-01-27 16:51:35', path: 'refresh-prod-code.md' },
+  { title: 'h5端web页面数据线上数据加载不到问题记录', date: '2021-05-25 17:05:58', path: 'can-not-get-data-onh5.md' },
+  { title: 'github 设置和取消代理', date: '2021-02-07 18:38:22', path: 'github-proxy.md' },
+  { title: '前端页面生成水印代码', date: '2020-06-18 17:02:18', path: 'water-mark.vue' },
+  { title: 'cordova安装目标android版本', date: '2020-06-18 17:02:18', path: 'cordova-install-target-android.md' },
+  { title: '原生js下载pdf链接文件而不是打开预览的解决方案', date: '2020-05-26 18:03:13', path: 'download-pdf.md' },
+  { title: '解决npm安装node-sass依赖过慢的问题', date: '2020-03-06 18:33:04', path: 'set-sass-binary-site.md' },
   {
     title: '解决momentjs使用webpack打包文件过大的问题',
     date: '2020-03-06 18:28:36',
     path: 'webpack-ignore-momentjs.md'
   },
+  { title: 'box-shadow实现内凹圆角', date: '2020-02-24 22:10:44', path: 'box-shadow-card.vue' },
   {
-    title: '解决npm安装node-sass依赖过慢的问题',
-    date: '2020-03-06 18:33:04',
-    path: 'set-sass-binary-site.md'
+    title: '修改 Mac Launchpad 图标的大小',
+    date: '2020-02-24 22:06:41',
+    description: '(大碗宽面MacBook版) 硬盘不大~, 但要还是要使劲儿下, 就算屏幕装不下~, 没关系还有这招啊🤪',
+    path: 'modify-mac-dock.md'
   },
-  {
-    title: '前端页面生成水印代码',
-    date: '2020-06-18 17:02:18',
-    path: 'water-mark.vue'
-  },
-  {
-    title: '原生js下载pdf链接文件而不是打开预览的解决方案',
-    date: '2020-05-26 18:03:13',
-    path: 'download-pdf.md'
-  },
-  {
-    title: 'box-shadow实现内凹圆角',
-    date: '2020-02-24 22:10:44',
-    path: 'box-shadow-card.vue'
-  },
-  {
-    title: '项目部署新版本后怎么提示用户刷新浏览器？',
-    date: '2022-01-27 16:51:35',
-    path: 'refresh-prod-code.md'
-  },
-  {
-    title: 'cordova安装目标android版本',
-    date: '2020-06-18 17:02:18',
-    path: 'cordova-install-target-android.md'
-  },
-  {
-    title: '记录一次线上 bug',
-    date: '2020-11-01 18:46:00',
-    path: 'an-online-bug.md'
-  },
-  {
-    title: 'h5端web页面数据线上数据加载不到问题记录',
-    date: '2021-05-25 17:05:58',
-    path: 'can-not-get-data-onh5.md'
-  },
-  {
-    title: 'JavaScript 中 this',
-    date: '2018-02-24 21:30:26',
-    path: 'this-in-javascript.md'
-  },
-  {
-    title: 'javascript中的数据传递',
-    date: '2018-02-24 22:01:46',
-    path: 'data-transmission.md'
-  },
-  {
-    title: 'javascript 中的执行上下文',
-    date: '2018-02-24 21:50:22',
-    path: 'javascript-context.md'
-  },
-  {
-    title: 'vue组件库共存开发导致编译错误的解决过程',
-    date: '2022-01-27 13:22:22',
-    path: 'vuejs-diff-version-bug.md'
-  },
-  {
-    title: '2021, 有喜有悲的一年',
-    date: '2022-01-31 00:13:22',
-    path: '2021-unhappy.vue'
-  },
-  {
-    title: 'github 设置和取消代理',
-    date: '2022-02-07 18:38:22',
-    path: 'github-proxy.md'
-  },
-  {
-    title: '动态设置elementui主题',
-    date: '2022-02-08 17:43:22',
-    path: 'element-ui-theme-set.vue',
-    description: h =>
-      h('div', [
-        h(
-          'p',
-          `element-ui的动态主题需求遇到很多次. vue-template-admin 项目中封装了一个 ThemePicker 是通过组件方式调用的(大佬就是强啊!)`
-        ),
-
-        h(
-          'p',
-          `但美中不足的是项目多的时候, 一旦涉及到修改, 就要同时修改好几个组件, 头疼的很, 再自己封装一个组件就很无聊, 所以干脆把大佬的代码封装成一个函数, 随时用随时调用, 包大小只有4kb`
-        ),
-        h('img', {
-          attrs: { height: '80px', src: 'https://tva1.sinaimg.cn/large/008i3skNgy1gz8i4tglttj308207z749.jpg' }
-        })
-      ])
-  }
-].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+  { title: 'webpack4 完整配置文件记录', date: '2020-02-24 17:51:12', path: 'webpack4-full-config.md' },
+  { title: 'vue组件库共存开发导致编译错误的解决过程', date: '2020-01-27 13:22:22', path: 'vuejs-diff-version-bug.md' },
+  { title: '记录一次线上 bug', date: '2019-11-01 18:46:00', path: 'an-online-bug.md' },
+  { title: 'javascript中的数据传递', date: '2018-02-24 22:01:46', path: 'data-transmission.md' },
+  { title: 'javascript 中的执行上下文', date: '2018-02-24 21:50:22', path: 'javascript-context.md' },
+  { title: 'JavaScript 中 this', date: '2018-02-24 21:30:26', path: 'this-in-javascript.md' }
+]
